5b63fd978cb6d82980dc755d24f9066e60168cb0
Mike Dilley <mike.dilley@gmail.com>
2018-11-27 19:41:43 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,5 @@ INSERT INTO subgenres (name) VALUES ("science-fiction"), ("fantasy");
 INSERT INTO authors (name) VALUES ("Jeff Vandermeer"), ("Madeleine L'Engle");
 
 INSERT INTO books (title, year, series_id)
-VALUES ("Annihilation", 2014, 1), ("Authority", 2014, 1), ("Acceptance", 2014, 1),;
+VALUES ("Annihilation", 2014, 1), ("Authority", 2014, 1), ("Acceptance", 2014, 1),
+       ("A Wrinkle in Time", 1962, 2), ("Wind in the Door", 1973, 1), ("Annihilation", 2014, 1);
