ea23e610c36819e50eb1cdc0f34855552abd01f5
Mike Dilley <mike.dilley@gmail.com>
2018-11-27 19:41:55 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,4 +6,4 @@ INSERT INTO authors (name) VALUES ("Jeff Vandermeer"), ("Madeleine L'Engle");
 
 INSERT INTO books (title, year, series_id)
 VALUES ("Annihilation", 2014, 1), ("Authority", 2014, 1), ("Acceptance", 2014, 1),
-       ("A Wrinkle in Time", 1962, 2), ("Wind in the Door", 1973, 1), ("Annihilation", 2014, 1);
+       ("A Wrinkle in Time", 1962, 2), ("Wind in the Door", 1973, 1), ("Annihilation", 1978, 1);
