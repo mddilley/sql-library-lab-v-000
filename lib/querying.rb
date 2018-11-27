42602b7ec9ffeeedbf6fba23bc4f08c213e54a88
Mike Dilley <mike.dilley@gmail.com>
2018-11-27 20:17:08 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,9 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT Books.title, Books.year FROM books
+   INNER JOIN Series ON Books.series_id = Series.id
+   WHERE Series.id = 1
+   ORDER BY Books.year;
+  "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
