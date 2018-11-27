30a7c2e225052c21ec3b966e2274db12a09e041c
Mike Dilley <mike.dilley@gmail.com>
2018-11-27 19:46:04 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,4 +8,6 @@ INSERT INTO books (title, year, series_id)
 VALUES ("Annihilation", 2014, 1), ("Authority", 2014, 1), ("Acceptance", 2014, 1),
        ("A Wrinkle in Time", 1962, 2), ("Wind in the Door", 1973, 2), ("Swiftly Tilting Planet", 1978, 2);
 
-INSERT INTO characters () VALUES ();
+INSERT INTO characters (name, motto, species, author_id, series_id)
+VALUES ("Biologist", "Plants are cool", "Human", 1, 1), ("Psycologist", "I like hypnosis", "Human", 1, 1),
+       ("Anthropologist", "I like to survey", "Human", 1, 1), ("Psycologist", "I like hypnosis", "Human", 1, 1);
