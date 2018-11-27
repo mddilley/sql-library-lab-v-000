fcb68eebd896315ddbd7ae9fca6f8d84ac6797fe
Mike Dilley <mike.dilley@gmail.com>
2018-11-27 20:46:05 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,8 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species) FROM Characters
-   GROUP BY species;
+   GROUP BY species
+   ORDER BY COUNT(species) DESC LIMIT 1;
    "
 end
 
