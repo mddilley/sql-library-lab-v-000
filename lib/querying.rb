5170ad300f7ed77ccd8a0082681ae6923d3d1633
Mike Dilley <mike.dilley@gmail.com>
2018-11-27 20:24:43 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT Characters.species, COUNT(Characters.species) FROM characters
+   ORDER BY COUNT(Characters.species) DESC LIMIT 1;
+   "
 end
 
 def select_name_and_series_subgenres_of_authors
