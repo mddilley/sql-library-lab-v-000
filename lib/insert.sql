4c6da56418729ba7b8df048814efa0a54e2f5569
Mike Dilley <mike.dilley@gmail.com>
2018-11-27 19:34:54 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,4 +4,4 @@ INSERT INTO series (title, author_id, subgenre_id) VALUES ("Time Quintet", 2, 2)
 INSERT INTO subgenres (name) VALUES ("science-fiction");
 INSERT INTO subgenres (name) VALUES ("fantasy");
 
-INSERT INTO author (name) VALUES ("Jeff Vandermeer"), ("Madeleine L'Engle");
+INSERT INTO authors (name) VALUES ("Jeff Vandermeer"), ("Madeleine L'Engle");
