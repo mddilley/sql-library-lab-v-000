4d42d8a75fa34fedd6679d520eef3dc06f559203
Mike Dilley <mike.dilley@gmail.com>
2018-11-27 19:36:26 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,7 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("The Southern Reach Trilogy", 1, 1), ("Time Quintet", 2, 2);
 
-
 INSERT INTO subgenres (name) VALUES ("science-fiction"), ("fantasy");
 
 INSERT INTO authors (name) VALUES ("Jeff Vandermeer"), ("Madeleine L'Engle");
+
+INSERT INTO books () VALUES ();
