78f23c9975de2fb63a6387f3d3b76f0ee88aeccc
Mike Dilley <mike.dilley@gmail.com>
2018-11-27 19:29:00 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("The Southern Reach Trilogy", 1, 1);
-INSERT INTO series (title, author_id, subgenre_id) VALUES ();
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Time Quintet", 2, 2);
 
-INSERT INTO subgenres (name) VALUES ();
-INSERT INTO subgenres (name) VALUES ();
+INSERT INTO subgenres (name) VALUES ("science-fiction");
+INSERT INTO subgenres (name) VALUES ("fantasy");
