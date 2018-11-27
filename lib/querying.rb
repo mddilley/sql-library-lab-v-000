a898423c57443e761964bac3278703f8874c8b49
Mike Dilley <mike.dilley@gmail.com>
2018-11-27 20:18:43 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,7 +7,8 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "
+  "SELECT Character.name, Character.motto FROM characters
+   ORDER BY Character.motto DESC LIMIT 1;
   "
 end
 
